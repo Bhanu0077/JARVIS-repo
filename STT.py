4c2d153726ee7539e911e6cd83a1bcd7f11c81e0
unknown <donadulabhanu@gmail.com>
2025-09-07 16:59:54 +0530

    Text To Speech Program

---
+++ STT.py
@@ -0,0 +1,22 @@
+import os 
+import time
+import pygame
+import speech_recognition as sr
+from gtts import gTTS
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception: " + str(e))
+        
+    return said
+
+print("Listening...")
+get_audio()
